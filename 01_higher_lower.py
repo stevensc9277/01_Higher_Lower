3a5dc3f6f5e9495a8e3517c74067a21b131e8fb3
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2020-03-05 11:23:17 +1300

    Update 01_higher_lower.py

--- 01_higher_lower.py
+++ 01_higher_lower.py
@@ -2,6 +2,7 @@ def num_check(question, low, high):
     valid = False
     while not valid:
         error = "Please enter a number between {} and {}".format(low, high)
+        print("")
         try:
             response = int(input(question))
             if low <= response <= high:
