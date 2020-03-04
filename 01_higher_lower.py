af47c1463ce59a58573a0a36a485a96193e02460
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2020-03-04 13:15:07 +1300

    Update 01_higher_lower.py

--- 01_higher_lower.py
+++ 01_higher_lower.py
@@ -1,1 +1,15 @@
-print("hi")
+def num_check(question, low, high):
+    valid = False
+    while not valid:
+        error = "Please enter a number between {} and {}".format(low, high)
+        try:
+            response = int(input(question))
+            if low <= response <= high:
+                return response
+            else:
+                print(error)
+                print()
+        except ValueError:
+            print(error)
+
+low_num = num_check("Enter a number between 1 and 10. ", 1, 10)
