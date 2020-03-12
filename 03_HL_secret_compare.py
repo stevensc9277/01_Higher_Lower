5e2a411479eac66d27c71e50a9800a77068943f1
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2020-03-13 10:56:00 +1300

    compare user guess and secret number

---
+++ 03_HL_secret_compare.py
@@ -0,0 +1,16 @@
+# HL component 3 - compares user guess with secret number
+
+SECRET = 7
+
+guess = ""
+
+while guess != SECRET:
+
+    guess = int(input("Guess: "))  # replace this with function call in due corse
+
+    if guess < SECRET:
+        print("Too high, try a  lower number")
+    elif guess > SECRET:
+        print("Too low, try a higher number")
+    else:
+        print("Congratulations! You found the secret number")
